46e125679dd73ff801efcb77301fc262e2151a82
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-02-10 02:15:57 +0530

    code added

---
+++ string methods.py
@@ -0,0 +1,9 @@
+x=input("enter string")
+print(len(x))
+
+y= "AnUshRaya SHarMa"
+print(len(y))        #counts number of characters(space is counted as a character)
+print(y.lower())     #converts into lowercase
+print(y.upper())     #converts into uppercase
+print(y.title())     #first character capital,other small
+print(y.count("a"))  #counts how many times a particular character is present
---
+++ string slicing.py
@@ -0,0 +1,10 @@
+from tkinter import E
+
+
+# language="python"
+# print(language[2:4])
+
+x="hello everyone"
+print(x[-3:14])
+print(x[1: ])
+print(x[ :3])
