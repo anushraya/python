2ee61fb4f98ad8fb4b06b559fdcd070544eba2f5
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-05-02 15:34:23 +0530

    Update functions 2.py

--- functions 2.py
+++ functions 2.py
@@ -1,4 +1,24 @@
+#function to print last character
 def last_char(name):
     return name[-1]
 name=input("enter name")
 print(last_char(name))
+
+
+#function to define odd_even
+def odd_even(num):
+    if num%2==0:
+        return"even"
+    return"odd"
+print(odd_even(4))
+
+#fuction defining even number
+def is_even(num):
+    if num%2==0:
+        return"true"
+print(is_even(10))
+
+#
+def song():
+    return"happy birthday"
+print(song())
