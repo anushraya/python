ee5df3ff95cb1ba3a0b8d214da3e60e72681308f
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-05-04 00:08:33 +0530

    Update variable_scope.py

--- variable_scope.py
+++ variable_scope.py
@@ -6,9 +6,37 @@
 # print(func())
 
 
-x=5  #global variable
+# x=5  #global variable
+# def func():
+#     x=2 #local variable
+#     return x
+# print(func())
+# print(x)
+
+
+#changing value of global variable x
+# x=5
+# def func():
+#     global x
+#     x=7   #local variable
+#     return x
+# print(func())
+# print(x)
+
+
+# x=5
+# def func():
+#     global x
+#     x=7   #local variable
+#     return x
+# print(x)
+# print(func())
+
+x=5
 def func():
-    x=2 #local variable
+    global x
+    x=7   #local variable
     return x
+print(x)
 print(func())
 print(x)
